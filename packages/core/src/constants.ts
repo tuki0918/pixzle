5875a19571c8cded424b7100e85ed951c0997020
Y.Yamamoto <tuki0918@gmail.com>
2025-12-03 22:40:30 +0900

    fix: update BLOCK_SIZE in DEFAULT_FRAGMENTATION_CONFIG to 8
    Updated the BLOCK_SIZE constant from 2 to 8 in the DEFAULT_FRAGMENTATION_CONFIG to improve fragmentation settings.

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -1,7 +1,7 @@
 export const MANIFEST_FILE_NAME = "manifest.json";
 
 export const DEFAULT_FRAGMENTATION_CONFIG = {
-  BLOCK_SIZE: 2,
+  BLOCK_SIZE: 8,
   PREFIX: "img",
   PRESERVE_NAME: false,
   CROSS_IMAGE_SHUFFLE: false,
